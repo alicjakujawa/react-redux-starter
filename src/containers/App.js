fc1d5add24c6786a3c31d3e543b22bb265a07d9c
Alicja Kujawa <alicja.kujawa@monterail.com>
2016-07-23 19:17:12 +0200

    Add redux dev tools

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
-import { combineReducers } from 'redux';
+import { combineReducers, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import CounterContainer from './CounterContainer';
-import { createStore, renderDevTools } from '../utils/devTools';
+import DevTools from './DevTools';
 
 import * as reducers from '../reducers';
 
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+let store
+if(__DEV__) {
+  const enhancer = compose(
+    DevTools.instrument()
+  );
+  store = createStore(reducer, enhancer);
+} else {
+  store = createStore(reducer);
+}
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Provider store={store}>
+      <Provider store={store}>
+        <div>
           <CounterContainer />
-        </Provider>
-
-        {renderDevTools(store)}
-      </div>
+          {__DEV__ && <DevTools />}
+        </div>
+      </Provider>
     );
   }
 }
